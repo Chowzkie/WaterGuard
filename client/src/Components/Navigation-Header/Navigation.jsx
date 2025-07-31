f23963a33f7ce8e20d9e165f740a9bfa4eb8d852
Chowzkie <rjf.chow00604@student.tsu.edu.ph>
2025-07-31 15:22:26 +0800

    add the off-canvas for the navigation to make it responsive

--- client/src/Components/Navigation-Header/Navigation.jsx
+++ client/src/Components/Navigation-Header/Navigation.jsx
@@ -1,56 +1,70 @@
 import { NavLink } from 'react-router-dom';
 import styles from "../../Styles/Navigation.module.css";
+import { useState } from 'react';
+import {Menu, X} from 'lucide-react'
 
 function Navigation() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
+
     return (
-        <div className={styles['component-wrapper-navigation']}>
+        <nav className={styles['main-nav-wrapper']}>
+            <button className={styles['hamburger-menu']} onClick={toggleMenu}>
+                {isOpen ? <X size={24} /> : <Menu size={24} />}
+            </button>
+            <div className={`${styles['component-wrapper-navigation']} ${isOpen ? styles['open'] : ''}`}>
+                <NavLink 
+                    to="/overview" 
+                    className={({ isActive }) =>
+                        `${styles['nav-link']} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    <div className={styles.navigation}>Overview</div>
+                </NavLink>
 
-            <NavLink 
-                to="/overview" 
-                className={({ isActive }) =>
-                    `${styles['nav-link']} ${isActive ? styles.active : ''}`
-                }
-            >
-                <div className={styles.navigation}>Overview</div>
-            </NavLink>
+                <NavLink 
+                    to="/dashboard" 
+                    className={({ isActive }) =>
+                        `${styles['nav-link']} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    <div className={styles.navigation}>Readings</div>
+                </NavLink>
 
-            <NavLink 
-                to="/dashboard" 
-                className={({ isActive }) =>
-                    `${styles['nav-link']} ${isActive ? styles.active : ''}`
-                }
-            >
-                <div className={styles.navigation}>Readings</div>
-            </NavLink>
+                <NavLink 
+                    to="/alerts" 
+                    className={({ isActive }) =>
+                        `${styles['nav-link']} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    <div className={styles.navigation}>Alerts</div>
+                </NavLink>
 
-            <NavLink 
-                to="/alerts" 
-                className={({ isActive }) =>
-                    `${styles['nav-link']} ${isActive ? styles.active : ''}`
-                }
-            >
-                <div className={styles.navigation}>Alerts</div>
-            </NavLink>
+                <NavLink 
+                    to="/devices" 
+                    className={({ isActive }) =>
+                        `${styles['nav-link']} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    <div className={styles.navigation}>Devices</div>
+                </NavLink>
 
-            <NavLink 
-                to="/devices" 
-                className={({ isActive }) =>
-                    `${styles['nav-link']} ${isActive ? styles.active : ''}`
-                }
-            >
-                <div className={styles.navigation}>Devices</div>
-            </NavLink>
-
-            <NavLink 
-                to="/configurations" 
-                className={({ isActive }) =>
-                    `${styles['nav-link']} ${isActive ? styles.active : ''}`
-                }
-            >
-                <div className={styles.navigation}>Configuration</div>
-            </NavLink>
-
-        </div>
+                <NavLink 
+                    to="/configurations" 
+                    className={({ isActive }) =>
+                        `${styles['nav-link']} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    <div className={styles.navigation}>Configuration</div>
+                </NavLink>
+            </div>
+        </nav>
     );
 }
 

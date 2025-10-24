8a448cdfffd12eb73e29c08d262110da15efba55
Chowzkie <rjf.chow00604@student.tsu.edu.ph>
2025-10-24 17:28:05 +0800

    Delete the Moock Folder

--- server/mockData/stations.js
+++
@@ -1,8 +0,0 @@
-// data/stations.js
-const FAKE_STATIONS_DATA = [
-    { id: 1, label: 'Pumping Station 1', location: 'Brgy. Bagong Bayan, Gerona, Tarlac', operation: 'On-going' },
-    { id: 2, label: 'Pumping Station 2', location: 'Brgy. Apsayan, Gerona, Tarlac', operation: 'On-going' },
-    { id: 3, label: 'Pumping Station 3', location: 'Brgy. Buenlang, Gerona, Tarlac', operation: 'Offline' }
-];
-
-module.exports = FAKE_STATIONS_DATA;

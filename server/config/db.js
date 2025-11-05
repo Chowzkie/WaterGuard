baa79555df7c68f58a48eae9c6637ccecceab534
Chowzkie <rjf.chow00604@student.tsu.edu.ph>
2025-11-05 17:28:38 +0800

    Change the database name into "WaterGuard"

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async() => {
     try{
-        await mongoose.connect("mongodb+srv://WaterGuard:p1hv6uI5qM6qK6Hu@cluster0.euoms4a.mongodb.net/User");
+        await mongoose.connect("mongodb+srv://WaterGuard:p1hv6uI5qM6qK6Hu@cluster0.euoms4a.mongodb.net/WaterGuard");
         console.log("MongoDB connected")
     }catch(err){
         console.error("Database connnection to MongoDB is failed", err.message);

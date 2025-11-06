9b1fb207be522b8a46203f190bcde498813efd29
Chowzkie <rjf.chow00604@student.tsu.edu.ph>
2025-11-06 21:51:03 +0800

    cahnge the port number

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const io = new Server(server, {
 });
 
 app.set("io", io); //
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
 /**
  * Main function to start the server.

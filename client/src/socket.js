de3dbde152be7599ee39681cffd8f798f39bf55e
Chowzkie <rjf.chow00604@student.tsu.edu.ph>
2025-11-06 22:48:59 +0800

    change port number

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const API_BASE_URL = "http://localhost:8080"; // adjust to your backend
+const API_BASE_URL = "http://localhost:4000"; // adjust to your backend
 
 const socket = io(API_BASE_URL, {
   transports: ["websocket"],
